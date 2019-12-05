48095a5c49787ca9ee98ffabce4b4ad42d147e2f
lander saavedra <landersaavedra@gmail.com>
2019-12-05 14:42:53 -0500

    commit

---
+++ SIGESDOC.Request/CargaComunicadoWord.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SIGESDOC.Request
+{
+    public class CargaComunicadoWord
+    {
+        public string FECHA_ACTUAL { get; set; }
+        public string NOM_DOC { get; set; }
+    }
+}
---
+++ SIGESDOC.Request/CargaInvitacionWord.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SIGESDOC.Request
+{
+    public class CargaInvitacionWord
+    {
+        public string FECHA_ACTUAL { get; set; }
+        public string NOM_DOC { get; set; }
+
+        public string ASUNTO { get; set; }
+
+    }
+}
